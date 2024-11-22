a6e14d0d7b92f873cd097895d6b046e96cce19e6
LucasLouvem <lucaslouvem2@gmail.com>
2024-11-22 19:03:29 -0300

    Atualizando as ferramentas

--- back-end/script.js
+++ back-end/script.js
@@ -1,8 +1,11 @@
-import express from express;
+import express from 'express';
 
 const app = express()
 
-app.listen(3000, async () => {
+app.listen(3000,() => {
     console.log("Servidor Online!");
 });
 
+app.get("/", (req, res)=>{
+    res.status(200).send("<h1>Bem Vindo</h1>")
+});
