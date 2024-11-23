89a46e3b90134a4661a5a5cb90c91dc06dbca66c
lucaslouvem2@gmail.com <lucaslouvem2@gmail.com>
2024-11-23 00:50:14 -0300

    Att

--- back-end/src/controlle/postControllers.js
+++ back-end/src/controlle/postControllers.js
@@ -1,5 +1,5 @@
-import routes from "../routes/postRoutes.js";
 import { getAllPosts, create } from "../models/postModel.js";
+import fs from "fs";
 
 export async function postList(req, res) {
     const posts = await getAllPosts();
@@ -10,6 +10,23 @@ export async function postAdd(req, res) {
     const newPost = req.body;
     try {
         const postCreated = await create(newPost);
+        res.status(200).json(postCreated);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({"Erro":"Falha na requisição"});
+    }
+}
+
+export async function postUpload(req, res) {
+    const newPost = {
+        descricao: "",
+        imagem: req.file.originalname,
+        alt: ""
+    };
+    try {
+        const postCreated = await create(newPost);
+        const archiveUpdate = `uploads/${postCreated.insertedId}.png`
+        fs.renameSync(req.file.path, archiveUpdate)
         res.status(200).json(postCreated);
     } catch (error) {
         console.error(error.message);
--- back-end/src/routes/postRoutes.js
+++ back-end/src/routes/postRoutes.js
@@ -1,5 +1,17 @@
 import express from "express";
-import { postAdd, postList } from "../controlle/postControllers.js";
+import multer from "multer";
+import { postAdd, postList, postUpload } from "../controlle/postControllers.js";
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null,'uploads/');
+    },
+    filename: function (req,file, cb){
+        cb(null,file.originalname)
+    }
+});
+
+const upload = multer({ dest: "./uploads", storage})
 
 const routes = (app) =>{
 
@@ -8,6 +20,8 @@ const routes = (app) =>{
     app.get("/posts", postList);
     
     app.post("/posts", postAdd);
+
+    app.post("/upload", upload.single("imagem"), postUpload);
 }
 
 export default routes
